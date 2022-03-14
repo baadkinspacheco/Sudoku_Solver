c8104b982180544b3da30e6be0154df9677c3125
baadkinspacheco <77076562+baadkinspacheco@users.noreply.github.com>
2022-03-13 19:07:54 -0700

    User can help solve the puzzle
    This version allows the user to add values to the grid if the program has run out of possibilities. Each time the user adds a new value to the grid, the program tries to complete the new version of the puzzle. It is assumed that the user is adding in correct values.

--- Sudoku Solver/PuzzleMain.java
+++ Sudoku Solver/PuzzleMain.java
@@ -17,11 +17,20 @@ public class PuzzleMain {
 		// Reads the file and puts the puzzle into 2D arrayList
 		List<List<Integer>> myPuzzle;
 		myPuzzle = readFile(args[0]);
-
-		// Iterate through the puzzle
+		
+		// Iterate through the puzzle and fill the puzzle with values
 		solve(myPuzzle);
+
+		// Checks to make sure puzzle is complete
+		// If not complete the user can add another value to complete puzzle
+		Scanner input = new Scanner(System.in);
+		while (isEmpty(myPuzzle) != true) {
+			tryAgain(myPuzzle, input);	
+		}
+		input.close();
 		
 		// Print out the puzzle
+		System.out.println();
 		printPuzzle(myPuzzle);
 }
 	
@@ -53,10 +62,40 @@ public class PuzzleMain {
 			return myPuzzle;
 			
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return null;
+	}
+	
+	/*
+	 * If the puzzle is not completed the user has the opportunity to add another value into the grid.
+	 * If the value is not valid or is zero the user can try again.
+	 * The result should be a completed puzzle.
+	 * @param is a 2D list of values in the puzzle
+	 * @param is a scanner object that will be used to read input from the user
+	 */
+	public static void tryAgain(List<List<Integer>> myPuzzle, Scanner input) {
+		
+		// Prints out the current puzzle
+		printPuzzle(myPuzzle);
+		System.out.println();
+	
+		
+		// User can add additional value if it is valid
+		System.out.println("Please enter another value in the form '(x,y) 8' to complete puzzle \n"
+				+ "where (0,0) is the top left of the grid");
+		
+		// Collects x,y, and value from the user
+		String newVal = input.nextLine();
+		String[] vals = newVal.split(" ");
+		int x = Integer.valueOf(String.valueOf(vals[0].charAt(1)));
+		int y = Integer.valueOf(String.valueOf(vals[0].charAt(3)));
+		int val = Integer.valueOf(String.valueOf(vals[1]));
+			
+		// Adds value to the grid
+		// Need to make a check that value is valid
+		myPuzzle.get(x).set(y, val);
+		solve(myPuzzle);
 	}
 	
 	/*
@@ -75,18 +114,43 @@ public class PuzzleMain {
 				}
 			}
 		}
-
 	}
 	
-	public static boolean isNotEmpty(List<List<Integer>> myPuzzle) {
-		// Checks our condition to make sure we dont't need to keep checking
-		for (int i = 0; i < ROW_SIZE; i++) {
+	/*
+	 * Another way to iterate through the grid recursively... not sure if want to use 
+	 */
+//	public static void solve2(List<List<Integer>> myPuzzle, int x, int y) {
+//		if (x <= ROW_SIZE && y <= COL_SIZE && x >= 0 && y >= 0 && myPuzzle.get(x).get(y) != 0) {
+//			return;
+//		}
+//		if (x <= ROW_SIZE && y <= COL_SIZE && x >= 0 && y >= 0 && myPuzzle.get(x).get(y) == 0) {
+//			insertNum(x, y, myPuzzle);
+//		}
+//		if (x <= ROW_SIZE) {
+//			solve2(myPuzzle, x + 1, y);
+//		}
+//		if (y <= COL_SIZE) {
+//			solve2(myPuzzle, x, y + 1);
+//		}
+//		if (x > ROW_SIZE) {
+//			solve2(myPuzzle, x - 1, y);
+//		}
+//		if (y > COL_SIZE) {
+//			solve2(myPuzzle, x, y - 1);
+//		}
+//	}
+	
+	/*
+	 * @param 2D list of values in the grid
+	 * @return boolean is true if there are no more empty spaces on the grid
+	 */
+	public static boolean isEmpty(List<List<Integer>> myPuzzle) {
+		for (int i = 0; i < ROW_SIZE; i++)
 			for (int j = 0; j < COL_SIZE; j++) {
 				if (myPuzzle.get(i).get(j) == 0) {
 					return false;
 				}
 			}
-		}
 		return true;
 	}
 	
@@ -101,20 +165,9 @@ public class PuzzleMain {
 		
 		int num = 0;
 		
-		// Iterate through the numbers 1-9
-		for (int i = 1; i < ROW_SIZE; i++) {
-			// Check if the number exists in the row
-			if (myPuzzle.get(row).indexOf(i) == -1) {
-				num = i;
-				// Check if the number exists in the column
-				// Subtracting 1 in the row because index starts at 0
-				// If number exists in the same column set number back to placeholder 0
-				if (myPuzzle.get(i - 1).get(col) == num) {
-					num = 0;
-				}
-			}
-				
-		}
+		// Returns value to be inserted based on rows and columns
+		num = possibleRowColumn(row, col, num, myPuzzle);
+
 		
 		// This means that the above test cases were passed
 		// The number does not exist in the row or the column
@@ -137,6 +190,36 @@ public class PuzzleMain {
 	
 	}
 	
+	/*
+	 * Finds possible integer between (1-9) to be inserted into the grid.
+	 * Checks numbers in the same rows and columns
+	 * @param row is the current row
+	 * @param col is the current column
+	 * @param num is the possible value to be inserted
+	 * @param myPuzzle is the 2D Array list of values on the grid
+	 * @return the possible value to be inserted at the (row,column) in the grid
+	 */
+	public static int possibleRowColumn(int row, int col, int num, List<List<Integer>> myPuzzle) {
+		// Iterate through the numbers 1-9
+		for (int i = 1; i < ROW_SIZE; i++) {
+			// Check if the number exists in the row
+			if (myPuzzle.get(row).indexOf(i) == -1) {
+				num = i;
+				// Check if the number exists in the column
+				// Subtracting 1 in the row because index starts at 0
+				// If number exists in the same column set number back to placeholder 0
+				if (myPuzzle.get(i - 1).get(col) == num) {
+					num = 0;
+				}
+			}	
+		}
+		return num;
+	}
+	
+	/*
+	 * Displays the puzzle in the console
+	 * @param is a 2D list of puzzle values
+	 */
 	public static void printPuzzle(List<List<Integer>> myPuzzle) {
 		for (List<Integer> line : myPuzzle) {
 			System.out.println(line);
